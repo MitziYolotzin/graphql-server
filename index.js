102db782614f273cbdedb4d2287304db1ddbd26f
MitziYolotzin <mitziulter@gmail.com>
2020-03-12 21:46:11 -0600

    query data, functions for id

--- index.js
+++ index.js
@@ -4,18 +4,43 @@ const app = express();
 const express_graphql = require('express-graphql');
 const { buildSchema} = require('graphql');
 
+//data
+const { courses } = require('./data.json');
+//console.log(courses);
+
+
 //que tipo de consulta puedo hacer a los datos
 //como se vera el schema de los datos
+//puede haber una consulta a una DB o archivo
+//query definiendo los datos, la consulta con el tipo de dato creado en el sevidor
 const schema = buildSchema(`
     type Query{
         message: String
+        course(id: Int!): Course
+        courses(topic: String): [Course]
     }
+
+    type Course {
+        id: Int
+        title: String
+        description: String
+        author: String
+        topic: String
+        url: String
+    }
+
 `);
 
-//que puede consultar desde root
+let getCourse = (args) => {
+    let id = args.id;
+}
+
+//que puede consultar desde root value
 //definir métodos a través de funciones
 const root = {
-    message: () => "hello"
+    //message: () => "hello"
+    course: getCourse
+    
 }
 
 //middleware
